1facf09fcd5f448b571aeb1f8e511504e81f93fe
“Nayan <nvbagale@yahoo.com>
2022-08-31 12:04:59 +0530

    minor changes

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-
+viewall()
+function rowselect(){
+    var trs = document.querySelectorAll("tr");
+    for (var i = 0; i < trs.length; i++)
+        (function (e) {
+        trs[e].addEventListener("click", function () {
+            console.log({
+            "FirstName": this.querySelectorAll("*")[0].innerHTML.trim(),
+            "LastName": this.querySelectorAll("*")[1].innerHTML.trim(),
+            });
+        }, false);
+    })(i);
+}
 
 function inputData(){
     let studID = document.getElementById("id").value
     let marks = document.getElementById("Marks").value
     if (studID != '' & marks !=''){
         insert(studID, marks)
+        postreq(studID, marks)
     }
 }
 
@@ -20,9 +33,26 @@ function defaultValue(){
 }
 
 async function viewall(){
-    const responce = await fetch("https://API.nayanbagale.repl.co/all")
+    const responce = await fetch("https://API.nayanbagale.repl.co")
     const json = await responce.json()
-    for(let i = 0; i <= json.length; i++){
-        insert(json[i].studID, json[i].marks)
+    console.log(json)
+    for(let i = 0; i < json.length; i++){
+        try {
+            insert(json[i].studID, json[i].marks)    
+        } catch (error) {
+            console.log(error)
+            
+        }
     }
 }
+
+function postreq(studID, marks){
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://API.nayanbagale.repl.co/post/json", true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({
+        "studID": studID,
+        "marks": marks
+    }));
+}
+
